4d7188e1ca9b57643a687c3655f9ea19d43214a3
Alvin Wang <alvin7@gmail.com>
2013-01-22 17:39:22 -0500

    Drew main menu

--- DigDug.js
+++ DigDug.js
@@ -1,10 +1,14 @@
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
-// Draw Title screen
 ctx.font = "55px Arial";
 ctx.textAlign = "center";
 ctx.fillText("Dig Dug", 300, 70);
+// Draw selectable options
 ctx.font = "40px Arial";
 ctx.fillText("Play", 300, 370);
 ctx.fillText("High Scores", 300, 450);
+// Draw selection rectangle
+ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+ctx.fillRect(170, 320, 250, 70);
+

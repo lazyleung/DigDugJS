ad12e7c4a4e02d8a15ccdb241cca8b2c0f5bf744
Ethan S. Chan <eschan@andrew.cmu,edu>
2013-01-28 18:49:09 -0500

    Fixed Dragon

--- dragon.js
+++ dragon.js
@@ -1,52 +1,55 @@
 function Dragon(dragonX, dragonY) {
-	this.x = dragonX;
-	this.y = dragonY;
-    this.width = 25;
-    this.height = 25;
-	this.speed = 1;
-	this.rightlimit = 600-this.width;
-	this.leftlimit = 0;
-	this.downlimit = 600-this.height;
-	this.uplimit = 0;
+    this.x = dragonX;
+    this.y = dragonY;
+        this.width = 25;
+        this.height = 25;
+    this.speed = 1;
+    this.rightlimit = 600-this.width;
+    this.leftlimit = 0;
+    this.downlimit = 600-this.height;
+    this.uplimit = 0;
+        this.image = new Image();
+        this.image.src = "digdugsprite.png";
 
-	this.moveRight = function() {
+    this.moveRight = function() {
                 futureX = this.x + blockSize/2;
-                if (spriteArray[getArrayPosition(futureX, this.y)] === null) {
+                if (overlay[getArrayPosition(futureX, this.y)] !== 0000) {
                         if(this.x + this.speed < this.rightlimit) {
                                 this.x += this.speed;
                         }
                 }
-        	
-	}
+            
+    }
 
-	this.moveLeft = function() {
-        	futureX = this.x - blockSize/2;
-                if (spriteArray[getArrayPosition(futureX, this.y)] === null) {
+    this.moveLeft = function() {
+            futureX = this.x - blockSize/2;
+                if (overlay[getArrayPosition(futureX, this.y)] !== 0000) {
                         if(this.x - this.speed > this.leftlimit) {
                                 this.x -= this.speed;
                         }
                 }
-	}
+    }
 
-	this.moveUp = function() {
-        	futureY = this.y - blockSize/2;
-                if (spriteArray[getArrayPosition(this.x, futureY)] === null) {
+    this.moveUp = function() {
+            futureY = this.y - blockSize/2;
+                if (overlay[getArrayPosition(this.x, futureY)] !== 0000) {
                         if(this.y - this.speed > this.uplimit) {
                                 this.y -= this.speed;
                         }
                 }
-	}
+    }
 
-	this.moveDown = function() {
-        	futureY = this.y + blockSize/2;
-                if (spriteArray[getArrayPosition(this.x, futureY)] === null) {
+    this.moveDown = function() {
+            futureY = this.y + blockSize/2;
+                if (overlay[getArrayPosition(this.x, futureY)] !== 0000) {
                         if(this.y + this.speed < this.downlimit) {
                                 this.y += this.speed;
                         }
                 }
-	}
+    }
 
-	this.update = function(player) {
+
+    this.update = function(player) {
                 var distX = Math.abs(player.x - this.x);
                 var distY = Math.abs(player.y - this.y);
                 if(distX < 200 && distY < 200) {
@@ -58,18 +61,92 @@ function Dragon(dragonX, dragonY) {
                                 this.moveUp();
                         else if(player.y > this.y)
                                 this.moveDown();
-                }	
-	}
-	
-	this.draw = function(ctx) {
-		var image = new Image();
-		image.src = "digdugsprite.png";
-		var x = this.x;
-		var y = this.y;
-		image.onload = function(){
-			ctx.drawImage(image, 314, 90, 14, 14, x, y, 25, 25);
-		}
-	}
+                }
+    }
+    
+    this.draw = function(ctx) {
+        var x = this.x;
+        var y = this.y;
+        ctx.drawImage(this.image, 314, 90, 14, 14, x, y, 25, 25);
+        
+    }
+
+        function getArrayPosition(x, y) {
+                return Math.floor((x + (blockSize/2)) / blockSize) + Math.floor((y + (blockSize/2)) / blockSize) * xGridSize;
+        }
+}
+function Blob(blobX, blobY) {
+    this.x = blobX;
+    this.y = blobY;
+        this.width = 25;
+        this.height = 25;
+    this.speed = 1;
+    this.rightlimit = 600-this.width;
+    this.leftlimit = 0;
+    this.downlimit = 600-this.height;
+    this.uplimit = 0;
+        this.image = new Image();
+        this.image.src = "digdugsprite.png";
+
+    this.moveRight = function() {
+                futureX = this.x + blockSize/2;
+                if (overlay[getArrayPosition(futureX, this.y)] !== 0000) {
+                        if(this.x + this.speed < this.rightlimit) {
+                                this.x += this.speed;
+                        }
+                }
+            
+    }
+
+    this.moveLeft = function() {
+            futureX = this.x - blockSize/2;
+                if (overlay[getArrayPosition(futureX, this.y)] !== 0000) {
+                        if(this.x - this.speed > this.leftlimit) {
+                                this.x -= this.speed;
+                        }
+                }
+    }
+
+    this.moveUp = function() {
+            futureY = this.y - blockSize/2;
+                if (overlay[getArrayPosition(this.x, futureY)] !== 0000) {
+                        if(this.y - this.speed > this.uplimit) {
+                                this.y -= this.speed;
+                        }
+                }
+    }
+
+    this.moveDown = function() {
+            futureY = this.y + blockSize/2;
+                if (overlay[getArrayPosition(this.x, futureY)] !== 0000) {
+                        if(this.y + this.speed < this.downlimit) {
+                                this.y += this.speed;
+                        }
+                }
+    }
+
+
+    this.update = function(player) {
+                var distX = Math.abs(player.x - this.x);
+                var distY = Math.abs(player.y - this.y);
+                if(distX < 200 && distY < 200) {
+                        if(player.x < this.x)
+                                this.moveLeft();
+                        else if(player.x > this.x)
+                                this.moveRight();
+                        if(player.y < this.y)
+                                this.moveUp();
+                        else if(player.y > this.y)
+                                this.moveDown();
+                }
+    }
+    
+    this.draw = function(ctx) {
+        var x = this.x;
+        var y = this.y;
+        ctx.drawImage(this.image, 332, 58, 14, 14, x, y, 25, 25);
+        
+    }
 
         function getArrayPosition(x, y) {
                 return Math.floor((x + (blockSize/2)) / blockSize) + Math.floor((y + (blockSize/2)) / blockSize) * xGridSize;
